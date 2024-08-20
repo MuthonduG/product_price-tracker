5bb2477a968c537ab99bd4e4772496dac429059a
MuthonduG <muthondugithinji@gmail.com>
2024-08-21 02:36:55 +0300

    initial commit for hero carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
 import Searchbar from '@/components/Searchbar'
+import HeroCarousel from '@/components/HeroCarousel'
 
 const Home = () => {
   return (
     <>
-      <section className='px-6 border-2 md:px-20 py-24 border-red-500'>
+      <section className='px-6 md:px-20 py-24'>
         <div className='flex max-xl:flex-col gap-16'>
           <div className='flex flex-col justify-center'>
             <p className='small-text'> 
@@ -23,7 +24,7 @@ const Home = () => {
             <Searchbar/>
           </div>
 
-          HeroCarousel
+          <HeroCarousel/>
         </div>
       </section>
 
