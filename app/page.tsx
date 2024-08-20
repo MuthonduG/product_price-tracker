735b37088c48e3e2a714fee387f94326fc9efbfd
MuthonduG <muthondugithinji@gmail.com>
2024-08-20 16:35:57 +0300

    initial commit for searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
+import Image from 'next/image'
+import Searchbar from '@/components/Searchbar'
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <section className='px-6 border-2 md:px-20 py-24 border-red-500'>
+        <div className='flex max-xl:flex-col gap-16'>
+          <div className='flex flex-col justify-center'>
+            <p className='small-text'> 
+              Smart shopping starts here:
+              <Image src="/assets/icons/arrow-right.svg" alt="img" width={16} height={16}/>
+            </p>
+            <h1 className='head-text'> 
+              Unleash the power of 
+              <span className='text-primary'> Price Tracking </span>
+            </h1>
+            <p className='mt-6'>
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
+
+            <Searchbar/>
+          </div>
+
+          HeroCarousel
+        </div>
+      </section>
+
+      <section className='trending-section'>
+        <h2 className='section-text'>Trending</h2>
+
+        <div className='flex flex-wrap gap-x-8 gap-y-16'>
+          {
+            ["Apple Iphone 15", "Book", "Sneakers"].map((product, index)=>{
+              return(
+                <div key={index}>{product}</div>
+              )
+            })
+          }
+        </div>
+      </section>
+    </>
   )
 }
 
