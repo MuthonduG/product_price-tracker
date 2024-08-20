07d0b96c7e10450b04cf7112467ee3f03c823845
MuthonduG <muthondugithinji@gmail.com>
2024-08-20 13:43:07 +0300

    update: added icons && logo for nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const navIcons = [
+  { src: '/assets/icons/search.svg', alt: 'search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'user' },
+]
 
 const Navbar = () => {
   return (
-    <div>
-      Nav
-    </div>
+    <header className='w-full'>
+      <nav className='nav'>
+        {/* Logo */}
+        <Link href="/" className='flex items-center gap-1'>
+          <Image src={'/assets/icons/logo.svg'} width={27} height={27} alt='logo'/>
+          <p className='nav-logo'>Price<span className='text-primary'>Tracker</span> </p>
+        </Link>
+        {/* Nav icons */}
+        <div className='flex items-center gap-5'>
+          {
+            navIcons.map((icon)=> {
+              return(
+                <Image key={icon.alt} src={icon.src} alt={icon.alt} width={28} height={28}/>
+              )
+            })
+          }
+        </div>
+      </nav>
+    </header>
   )
-}
+};
 
-export default Navbar
+export default Navbar;
